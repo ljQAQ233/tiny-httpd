75a1b4c963f32aef5cd7010b8f88809a31bb45e3
maouai233 <maouai233@outlook.com>
2025-03-09 11:43:26 +0800

    feat: static files access supported (with path check),       a 404 page is provided!!!

--- main.c
+++ main.c
@@ -7,11 +7,14 @@
         
 char buf[4096];
 
+#define MAX_PATH 128
+
 struct request
 {
     char *buf;
     char method[8];
-    char path[128];
+    char path[MAX_PATH];
+    char qury[MAX_PATH];
     char proto[16];
     char host[128];
     char agent[64];
@@ -49,6 +52,7 @@ int tiny_rp(int err, char *type, int len, void *body)
 }
 
 char *tiny_ver = "tiny/1.0";
+char tiny_root[MAX_PATH]; // site root path
 
 int tiny_err(int err)
 {
@@ -75,6 +79,47 @@ int xmeth(struct request *r)
 
 int xpath(struct request *r)
 {
+    char *p = r->path;
+    char *q = r->path;
+
+    char *qry = NULL;
+
+    while (*p)
+    {
+        if (*p == '+')
+        {
+            *q = ' ';
+            p += 1;
+        }
+        else if (*p == '%')
+        {
+            char hex[3];
+            hex[0] = *(p + 1);
+            hex[1] = *(p + 2);
+            hex[2] = 0;
+            *q = (char)strtol(hex, NULL, 16);
+            p += 1 + 2;
+        }
+        else if (*p == '?')
+        {
+            *q = 0;
+            qry = q+1;
+            p += 1;
+        }
+        else
+        {
+            *q = *p;
+            p += 1;
+        }
+        q += 1;
+    }
+    *q = 0;
+
+    if (qry)
+    {
+        strncpy(r->qury, qry, sizeof(r->qury));
+    }
+
     return 0;
 }
 
@@ -85,6 +130,80 @@ int parse(char *buf, struct request *r)
             r->method,
             r->path,
             r->proto);
+
+    xmeth(r); // test if method is supported
+    xpath(r); // get path separated
+    return 0;
+}
+
+#include <sys/stat.h>
+
+int acces(char *real, char *path)
+{
+    char tmp[MAX_PATH];
+    sprintf(tmp, "site" "%s", path);
+    realpath(tmp, real);
+    if (strncmp(real, tiny_root, strlen(tiny_root)))
+        return -1;
+
+    return 0;
+}
+
+#include <fcntl.h>
+#include <sys/mman.h>
+
+char *stype(char *file)
+{
+    char *type = "text/plain";
+
+    if (strstr(file, ".html"))
+        type = "text/html";
+    else if (strstr(file, ".gif"))
+        type = "image/gif";
+    else if (strstr(file, ".png"))
+        type = "image/png";
+    else if (strstr(file, ".jpg"))
+        type = "image/jpeg";
+
+    return type;
+}
+
+int serve(struct request *r)
+{
+    // static files
+    char path[MAX_PATH];
+    if (acces(path, r->path) < 0)
+    {
+        tiny_err(403);
+        return -1;
+    }
+
+    struct stat sb;
+    if (stat(path, &sb) < 0)
+    {
+        tiny_err(404);
+        return -1;
+    }
+
+    printf("GET %s\n", path);
+
+    int len = sb.st_size;
+    int fd = open(path, O_RDONLY);
+    if (fd < 0)
+    {
+        tiny_err(404);
+        return -1;
+    }
+
+    void *buf = mmap(
+            NULL, len,
+            PROT_READ, MAP_PRIVATE,
+            fd, 0);
+    char *type = stype(path);
+    tiny_rp(200, type, len, buf);
+
+    munmap(buf, len);
+    close(fd);
     return 0;
 }
 
@@ -108,12 +227,12 @@ int tiny_main(int port)
 
     for (;;)
     {
-        printf("recv...\n");
         cu = accept(sr, NULL, NULL);
         read(cu, buf, sizeof(buf));
 
-        tiny_err(200);
-
+        struct request r;
+        parse(buf, &r);
+        serve(&r);
         close(cu);
     }
 
@@ -125,8 +244,11 @@ die:
 
 int main(int argc, char *argv[])
 {
+    getcwd(tiny_root, sizeof(tiny_root));
+    strcat(tiny_root, "/site");
+
     if (argc != 2)
-        return 1;
+        return tiny_main(8082);
 
     return tiny_main(atoi(argv[1]));
 }
